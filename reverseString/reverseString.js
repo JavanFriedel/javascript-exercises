4807a45d9b5dc602cea839659d7042dc64250ef0
JavanFriedel <JavanFriedel@hotmail.com>
2021-06-14 17:55:26 -0400

    Completed reverseString with all tests passing

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let startArray = string.split('');
+
+    let newArray = []
+
+    for (let i = startArray.length - 1; i >= 0; i--){
+        newArray.push(startArray[i])
+    }
+
+    return newArray.join('')
+
 
 };
 
