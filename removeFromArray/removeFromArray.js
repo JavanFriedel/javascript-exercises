745ee2be1f91005b4bf4928187744764ab0ee0eb
JavanFriedel <JavanFriedel@hotmail.com>
2021-06-14 19:10:28 -0400

    removeFromArray project completed with all tests passing

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let testArray = args[0]
 
+    for (let i = 1; i < args.length; i++){
+        let argPos = testArray.indexOf(args[i])
+
+        if (argPos == -1){
+            continue
+        }
+        testArray.splice(argPos, 1)
+    }
+
+    return testArray
 };
 
 module.exports = removeFromArray;
