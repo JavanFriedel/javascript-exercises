3cdc98502d7e12074d9f9e4a64fe6000bfa3a01f
JavanFriedel <JavanFriedel@hotmail.com>
2021-06-14 19:22:59 -0400

    sumAll project complete with all tests passing

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0;
 
+    if (start < 0 || finish < 0){
+        return 'ERROR'
+    }
+
+    if (typeof(start) != 'number' || typeof(finish) != 'number'){
+        return 'ERROR'
+    }
+
+    let small;
+    let large;
+
+    if (start > finish) {
+        large = start;
+        small = finish;
+    } else {
+        large = finish;
+        small = start;
+    }
+
+    for (let i = small; i <= large; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 module.exports = sumAll;
